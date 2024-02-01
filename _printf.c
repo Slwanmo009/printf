543e0a10fb3f8374e326f06b6fbff1df1bf6ea0c
Bakry200 <bakryalfadul@gmail.com>
2024-02-01 00:57:53 -0800

    edit2

--- _printf.c
+++ _printf.c
@@ -11,26 +11,21 @@ int _printf(const char *format, ...)
 	va_list ap;
 	char *p;
 	char *start;
-
 	params_t params = PARAMS_INIT;
 
 	va_start(ap, format);
-
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
-	       return (-1);
+		return (-1);
 	for (p = (char *)format; *p; p++)
 	{
-	
 		init_params(&params, ap);
 		if (*p != '%')
 		{
 			sum += _putchar(*p);
 			continue;
 		}
-
 		start = p;
 		p++;
 		while (get_flag(p, &params))
