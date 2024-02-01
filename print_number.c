4492b60de6a040f0da7ec46c5dfdb652ecfa8463
Bakry200 <bakryalfadul@gmail.com>
2024-02-01 08:55:19 -0800

    kkj

--- print_number.c
+++ print_number.c
@@ -6,19 +6,20 @@
  */
 int _isdigit(int c)
 {
-        return (c >= '0' && c <= '9');
+	return (c >= '0' && c <= '9');
 }
 /**
  * _strlen - return the length of a string
  * @s: the string whose length of string
+ * Return: chars printed
  */
 int _strlen(char *s)
 {
-        int i = 0;
+	int i = 0;
 
-        while (*s++)
-                i++;
-        return (i);
+	while (*s++)
+		i++;
+	return (i);
 }
 /**
  * print_number - print number with options
@@ -70,18 +71,18 @@ int print_number_right_shift(char *str, params_t *params)
 	if ((params->plus_flag && !neg2) ||
 	(!params->plus_flag && params->space_flag && !neg2))
 		i++;
-        if (neg && pad_char == '0')
-        n += _putchar('-');
-        if (params->plus_flag && !neg2 && pad_char == '0' && !params->unsign)
-        n += _putchar('+');
-        else if (!params->plus_flag && params->space_flag && !neg2 &&
-                        !params->unsign && params->zero_flag)
-        n += _putchar(' ');
-        while (i++ < params->width)
+	if (neg && pad_char == '0')
+		n += _putchar('-');
+	if (params->plus_flag && !neg2 && pad_char == '0' && !params->unsign)
+		n += _putchar('+');
+	else if (!params->plus_flag && params->space_flag && !neg2 &&
+			!params->unsign && params->zero_flag)
+		n += _putchar(' ');
+	while (i++ < params->width)
 		n += _putchar(pad_char);
 	if (neg && pad_char == ' ')
-        n += _putchar('-');
-        if (params->plus_flag && !neg2 && pad_char == ' ' && !params->unsign)
+		n += _putchar('-');
+	if (params->plus_flag && !neg2 && pad_char == ' ' && !params->unsign)
 		n += _putchar('+');
 	else if (!params->plus_flag && params->space_flag && !neg2 &&
 			!params->unsign && !params->zero_flag)
--- specifiers.c
+++ specifiers.c
@@ -12,6 +12,15 @@ int (*get_specifier(char *s))(va_list ap, params_t *params)
 		{"i", print_int},
 		{"s", print_string},
 		{"%", print_percent},
+		{"b", print_binary},
+		{"o", print_octal},
+		{"u", print_unsigned},
+		{"x", print_hex},
+		{"X", print_HEX},
+		{"p", print_address},
+		{"S", print_S},
+		{"r", print_rev},
+		{"R", print_rot13}'
 		{NULL, NULL}
 	};
 	int i = 0;
